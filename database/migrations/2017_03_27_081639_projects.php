f7aa0a028e869b48ee01ba00fd0094b5b9c95be4
Alexey Dobrynin <ag.dobrynin@gmail.com>
2017-03-27 14:05:32 +0400

    Nothing

--- database/migrations/2017_03_27_081639_projects.php
+++ database/migrations/2017_03_27_081639_projects.php
@@ -17,7 +17,7 @@ class Projects extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description');
-            $table->timestamp('created_at');
+            $table->timestamps();
         });
     }
 
